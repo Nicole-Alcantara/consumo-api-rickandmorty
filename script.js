1ae74e793817bab873308a44118c162ea3f74b0e
Nicole-Alcantara <alcantara.nicole.jesus@gmail.com>
2025-08-20 08:50:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,29 @@ async function fetchCharacters(page){
 
     try{
         const response = await fetch(`https://rickandmortyapi.com/api/character`)
-        const data = await response.jason()
-        console.log(data)
+        const data = await response.json()
+        //console.log(data)
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Página inválida! Tente outra. (1/42)</p>"
+            return
+        }
+
+        resultsDiv.innerHTML = ""
+        data.results.forEach(character => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${character.image}" alt="${character.name}">
+                <h3>${character.name}</h3>
+                <p><strong>Status:<strong>${character.status}</p>
+                <p><strong>Espécie:<strong>${character.species}</p>
+            `
+            resultsDiv.appendChild(card)
+        })
+
     } catch(error) {
-    
+        // console.log("deu ruim")
     }
 }
 
+fetchCharacters(1)
